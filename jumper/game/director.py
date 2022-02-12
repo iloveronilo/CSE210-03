1588e5fb3caf49fce43cd22a6c8ed32302274935
Alan MONTOYA <alanmontoya@felipemotta.com>
2022-02-12 12:08:35 -0500

    Method Game Over added

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -13,6 +13,8 @@ class Director:
         self._game_title = GameTitle()
         self._guessed = False
         self._user_guess = ""
+        self._indices = []
+        self._index = 0
         self._tries = 0
 
     def startGame(self):
@@ -20,8 +22,7 @@ class Director:
         while not self._guessed and self._tries < 6:
             self._display()
             self._wordTracker()
-            self._addLetter()
-            self._slicer()
+            self._gameOver()
 
     def _display(self):
         """Display in the console the flow of the
@@ -29,11 +30,6 @@ class Director:
         """
         self._game_title.clearConsole()
         self._game_title.PrintGameTitle("Jumper")
-        self.guess_word = self._sliced_word.slicer()
-        self.covered_word = "_ " * len(self.guess_word)
-        print(f"Covered word: {self.covered_word} - Uncovered word: {''.join(self.guess_word)}")
-        self._parachuteTracker.parachuteChooser(self._tries)
-        self._is_playing = True
         # self._tries = 6
 
     # Logic of the game
@@ -42,18 +38,46 @@ class Director:
         that the user what to guess from the jumper
         Also it update the jumper graph
         """
-        self._user_guess = input("Please guess a letter or the word: ").upper()
+        self._parachuteTracker.parachuteChooser(self._tries)
+        self._is_playing = True
+        self.guess_word = self._sliced_word.slicer()
+        self.covered_word = "_" * len(self.guess_word)
+        self.uncovered_word = list(self.covered_word)
+        print(f"Covered word: {self.covered_word} - Uncovered word: {''.join(self.guess_word)}")
+        self._user_guess = input("\nPlease guess a letter or the word: ").upper()
         if len(self._user_guess) == 1 and self._user_guess.isalpha():
             if self._user_guess not in self.guess_word:
                 print(f"Sorry, {self._user_guess} is not in the word")
                 self._tries += 1
                 print(self._tries)
+            else:
+                print(f"Awesome, {self._user_guess} is in the word")
+                for letter in self.guess_word:
+                    if self._user_guess == letter:
+                        self._indices.append(self._index)
+                        self._index += 1
+                    else:
+                        self._index += 1
+                for i in range(len(self.covered_word)):
+                    if i in self._indices:
+                        self.uncovered_word[i] = self._user_guess
+                print(f"{''.join(self.uncovered_word)}")
         pass
 
 
     # Method to remove
-    def _addLetter(self):
-        pass
-    # Method to remove
-    def _slicer(self):
-        pass
+    def _gameOver(self):
+        if self._tries >= 5:
+            self._guessed = True
+        self._display()
+        print("""
+        
+   ____    _    __  __ _____    _____     _______ ____  
+  / ___|  / \  |  \/  | ____|  / _ \ \   / / ____|  _ \ 
+ | |  _  / _ \ | |\/| |  _|   | | | \ \ / /|  _| | |_) |
+ | |_| |/ ___ \| |  | | |___  | |_| |\ V / | |___|  _ < 
+  \____/_/   \_\_|  |_|_____|  \___/  \_/  |_____|_| \_\\
+                                                      
+
+        """)
+    
